cfc804fbf097a0c8a9969a7a4e3b7806a272e2e9
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-09-30 02:18:08 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import TabTest from "./components/custom-tabs/tabs-test";
+function App() {
+  return (
+    <div className="App">
+      {/* Custom tab Component */}
+
+      <TabTest />
+    </div>
+  );
+}
+
+export default App;
